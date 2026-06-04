5e6062e8f24975bd2c21e2ff0946b4aefe118b74
duairfan70154622 <70154622@student.uol.edu.pk>
2026-06-04 20:44:04 +0500

    Add files via upload

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 
-MONGO_URL = "mongodb+srv://70154622_db_user:RRumaLiHhWzLtFww@cluster0.pcwxg0l.mongodb.net/?appName=Cluster0"
+MONGO_URL = "mongodb+srv://70154622_db_user:jXcPs5Jhnrd7xM2u@cluster0.pcwxg0l.mongodb.net/?appName=Cluster0"
 
 client = MongoClient(MONGO_URL)
 
